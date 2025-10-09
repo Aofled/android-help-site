edbd60498cc6b23011fe17ace5c78e4e7c1fceed
Serhey A <madara_08@mail.ru>
2025-10-09 17:06:35 +0300

    feat:  update menu creation functionality
    - Add content category selection
    - Remove field for the JSON file name: name is formed from the file link
    - Update design

--- generator/js/content/menu-form.js
+++ generator/js/content/menu-form.js
@@ -2,38 +2,30 @@ export function initMenuForm() {
   const addButton = document.getElementById('add-to-json');
   const jsonTextarea = document.getElementById('json-input');
   const urlInput = document.getElementById('menu-url');
-  const contentFileInput = document.getElementById('menu-content-file');
-
-  urlInput.addEventListener('blur', () => {
-    if (urlInput.value && !urlInput.value.startsWith('#')) {
-      urlInput.value = '#' + urlInput.value;
-    }
-  });
-
-  contentFileInput.addEventListener('blur', () => {
-    if (contentFileInput.value && !contentFileInput.value.endsWith('.json')) {
-      contentFileInput.value = contentFileInput.value.replace(/.json$/, '') + '.json';
-    }
-  });
 
   addButton.addEventListener('click', () => {
     const title = document.getElementById('menu-title').value.trim();
-    let url = document.getElementById('menu-url').value.trim();
-    let contentFile = document.getElementById('menu-content-file').value.trim();
+    const urlSlug = urlInput.value.trim();
     const hasSubItems = document.getElementById('menu-has-subitems').checked;
+    const platform = document.querySelector('input[name="platform"]:checked').value;
 
     if (!title) {
       alert('Пожалуйста, укажите title');
       return;
     }
 
-    if (url && !url.startsWith('#')) url = '#' + url;
-    if (contentFile && !contentFile.endsWith('.json')) contentFile += '.json';
+    if (!urlSlug) {
+      alert('Пожалуйста, укажите URL Slug');
+      return;
+    }
+
+    const url = `#${platform}-${urlSlug}`;
+    const contentFile = `${platform}-${urlSlug}.json`;
 
     const menuItem = {
       title,
-      ...(url && { url }),
-      ...(contentFile && { contentFile }),
+      url,
+      contentFile,
       ...(hasSubItems && { subItems: [] })
     };
 
